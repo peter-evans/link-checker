3ca0edddc2814b58ba1d06d955bbad44650b20c3
Sebass van Boxel <svboxel@gmail.com>
2019-12-10 10:23:16 +0100

    Set output in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,15 @@ mkdir -p /tmp/link-checker
 # Execute Liche
 liche $* >/tmp/link-checker/out 2>&1
 
-# If link errors were found output a report to the designated directory
+# If link errors were found output them to the action and to a report to the designated directory
 if [ $? -eq 1 ]; then
+    result=$(cat /tmp/link-checker/out)
+    echo "::set-output name=result::$result"
+
     mkdir -p $LINKCHECKER_OUTPUT_DIR
     echo -e '### Link Checker\nErrors were reported while checking the connectivity of links.\n```' \
         >$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
-    cat /tmp/link-checker/out >>$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
+    result >>$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
     echo '```' >>$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
     echo "Link checker output file: $LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME"
 fi
